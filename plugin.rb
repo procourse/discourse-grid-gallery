374d49b3776bd4552ffb3b68991e453cd08d7824
Joe Buhlig <hello@joebuhlig.com>
2018-05-14 10:08:40 -0500

    Clean up i18N and category setting

--- plugin.rb
+++ plugin.rb
@@ -1,23 +1,40 @@
-# name: discourse-blendernation-grid-gallery
+# name: discourse-grid-gallery
 # about: A plugin to display post images in a grid.
 # version: 0.0.1
 # authors: ProCourse
+# url: https://github.com/procourse/discourse-grid-gallery
 
-enabled_site_setting :tags_displaying_gallery
-enabled_site_setting :desktop_picture_width
-enabled_site_setting :mobile_picture_width
+enabled_site_setting :grid_gallery_enabled
 
 after_initialize do
-    class ::Guardian
+    class ::Category
         def self.reset_grid_view_cache
-          @@allowed_accepted_cache["allowed"] =
+          @grid_view_default_cache["grid_default"] =
             begin
               Set.new(
                 CategoryCustomField
-                  .where(name: "enable_grid_for_category", value: "false")
+                  .where(name: "enable_grid_view", value: "false")
                   .pluck(:category_id)
               )
             end
+        end
+
+        @grid_view_default_cache = DistributedCache.new("grid_default")
+
+        def self.grid_default?(category_id)
+          return false unless SiteSetting.grid_gallery_enabled
+
+          unless set = @grid_view_default_cache["grid_default"]
+            set = reset_grid_view_cache
+          end
+          set.include?(category_id)
+        end
+
+        after_save :reset_grid_view_cache
+
+        protected
+        def reset_grid_view_cache
+          ::Category.reset_grid_view_cache
         end
     end
 end
